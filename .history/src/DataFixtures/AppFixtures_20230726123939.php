30dcf60945cf3f54a8f6cf3a90f38995403b8e54
Gitubeuse92 <amanda_v_f@hotmail.com>
2023-07-26 12:58:25 +0200

    Fixtures + AccueilController

---
+++ .history/src/DataFixtures/AppFixtures_20230726123939.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Faker\Factory;
+use App\Entity\Author;
+use App\Entity\Article;
+use App\Entity\Category;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create('fr_FR');
+
+        //Catégories       
+        $str_categories = [
+            'Sorties',
+            'Histoire',
+            'Gastronomie',
+            'Premium'
+        ];
+
+        $obj_categories = array(count($str_categories));
+        
+        for ($i = 0; $i < count($str_categories); $i++) {
+            $category = new Category();
+            $category->setName($str_categories[$i]);
+            $manager->persist($category);
+            $obj_categories[$i] = $category;
+        }
+        //Articles
+        for ($i = 0; $i < 6; $i++) {
+            //Auteurs
+            $auteur = new Author();
+            $auteur->setNom($faker->name());
+            $manager->persist($auteur);
+
+            //Articles
+            $article = new Article();
+            $article->setTitle($faker->sentence($nbWords = 10, $variableNbWords = true));
+            $article->setSubTitle($faker->sentence($nbWords = 20, $variableNbWords = true));
+            $article->setContent($faker->paragraph(6));
+            $article->setCategory($obj_categories[$i % count($obj_categories)]);
+            $article->setAuthor($auteur);
+            $manager->persist($article);
+        }
+
+        // $manager->persist($product);
+        $manager->flush();
+    }
+}
+
--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\ArticleRepository;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: ArticleRepository::class)]
@@ -17,6 +18,19 @@ class Article
     #[ORM\JoinColumn(nullable: false)]
     private ?Author $author = null;
 
+    #[ORM\Column(length: 40)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $content = null;
+
+    #[ORM\ManyToOne(inversedBy: 'articles')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Category $category = null;
+
+    #[ORM\Column(length: 50, nullable: true)]
+    private ?string $subtitle = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -30,6 +44,54 @@ class Article
     public function setAuthor(?Author $author): static
     {
         $this->author = $author;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(?string $content): static
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): static
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    public function getSubtitle(): ?string
+    {
+        return $this->subtitle;
+    }
+
+    public function setSubtitle(?string $subtitle): static
+    {
+        $this->subtitle = $subtitle;
 
         return $this;
     }
